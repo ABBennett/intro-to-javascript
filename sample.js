04e06480e67e97cd61d14cab4bb5c002c85e0199
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-27 12:27:36 -0500

    js age prompt

--- sample.js
+++ sample.js
@@ -1,1 +1,9 @@
-prompt("Hello");
+var age = prompt("Hello what is your age?");
+
+if (age >= 21) {
+  var beverage = "BEER";
+} else {
+  var beverage = "Sprite";
+}
+
+prompt("I will server you " + beverage);
